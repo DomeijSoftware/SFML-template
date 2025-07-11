aa8898102644068feb5831ea4d08102d6de8aa08
DomeijSoftware <adomeij@proton.me>
2025-07-11 10:14:52 +0200

    a

---
+++ shaders/Vert/test.vert
@@ -0,0 +1,18 @@
+//test.vert
+
+attribute vec4 position;
+attribute vec2 texCoord;
+attribute vec4 color;
+
+
+void main()
+{
+    // transform the vertex position
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+
+    // transform the texture coordinates
+    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+
+    // forward the vertex color
+    gl_FrontColor = gl_Color;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
+// main.pcc
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+constexpr float PI = 3.1415f;
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    window.setFramerateLimit(60);
+    sf::CircleShape circle(100.f);
+    sf::Texture texture;
+    if (!texture.loadFromFile("../../textures/John-Monsterhunter.jpg", false))
+    {
+        std::cout << "Error loading textures" << std::endl;
+
+        return -1;
+    }
+
+    circle.setTexture(&texture);
 
     while (window.isOpen())
     {
@@ -16,6 +30,7 @@ int main()
         }
 
         window.clear();
+        window.draw(circle);
         window.display();
     }
 }
